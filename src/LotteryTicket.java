663ebf454cb4118258496d4502aa4b653553e9ef
hiya-s <sharmahiya1234@gmail.com>
2022-05-31 11:50:31 -0500

    Initial commit

---
+++ src/LotteryTicket.java
@@ -0,0 +1,39 @@
+
+public class LotteryTicket {
+	public static void main(String [] args) {
+		int [][] lotteryCard = {{20,15,7},
+								{8, 7, 19},
+								{7, 13, 47}
+		};							
+		int arr[] = new int [5];
+		int l =0;
+		for(int h =0; h <arr.length; h++) {
+			arr[h]= l;
+			System.out.println(arr[h]);
+			l++;
+			
+		}
+		
+								
+	/*
+		int lotteryCard2[][] = new int[3] [3];
+		lotteryCard2[0][0]= 20;
+		lotteryCard2[0][1]= 15;
+		lotteryCard2[0][2]= 7;
+		lotteryCard2[1][0]= 8;
+		lotteryCard2[1][1]= 7;
+		lotteryCard2[1][2]= 19;
+		lotteryCard2[2][0]= 7;
+		lotteryCard2[2][1]= 13;
+		lotteryCard2[2][2]= 47;
+		
+		for(int i =0; i <3; i++) {
+			for(int j =0; j<3; j++) {
+				System.out.println(lotteryCard2[i][j]);}
+				System.out.println("----------------");
+		}
+		
+		}*/
+		
+}
+}
